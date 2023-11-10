ca7090fdd03b15e25ff26a26ab99f27397d945cf
Przemyslaw Anton Arciszewski <przemyslawapl@gmail.com>
2023-11-10 20:24:47 +0100

    Further development

---
+++ resources/card_data.gd
@@ -0,0 +1,56 @@
+extends  Node
+#Template
+#"000" : {
+#		"name" : "Text here",
+#		"description : "Text here",
+#		"image" : "path to image",
+#		"tags" : {
+#				"one-off" : true/false,
+#				"serial" : next card id or null,
+#		} ,
+#		"effect" : {
+#				"one": 0,
+#				"two": 0,
+#				"three": 0,
+#				"four": 0,
+#		}
+#	},
+#}
+
+
+
+
+
+
+var cards: Dictionary = { 
+	"001" : {
+		"name" : "Joe",
+		"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor viverra ex vel pretium. Donec nec porta leo. Phasellus pulvinar, sapien sed consectetur egestas, enim enim faucibus odio, eu semper.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor viverra ex vel pretium. Donec nec porta leo. Phasellus pulvinar, sapien sed consectetur egestas, enim enim faucibus odio, eu semper.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor viverra ex vel pretium. Donec nec porta leo. Phasellus pulvinar, sapien sed consectetur egestas, enim enim faucibus odio, eu semper.",
+		"image" : "res://assets/visual/Person_icon_BLACK-01.svg.png",
+		"tags" : {
+				"one-off" : false,
+				"serial" : null,
+		} ,
+		"effect" : {
+				"one": 0,
+				"two": 0,
+				"three": 0,
+				"four": 0,
+		}
+	},
+	"002" : {
+		"name" : "Mike",
+		"description" : "Ég fokking elska gæsir og geitur maður, þær eru það fallegasta í þessum heimi, sapien sed consectetur egestas, enim enim faucibus odio, eu semper.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor viverra ex vel pretium. Donec nec porta leo. Phasellus pulvinar, sapien sed consectetur egestas, enim enim faucibus odio, eu semper.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor viverra ex vel pretium. Donec nec porta leo. Phasellus pulvinar, sapien sed consectetur egestas, enim enim faucibus odio, eu semper.",
+		"image" : "res://assets/visual/output-onlinepngtools.png",
+		"tags" : {
+				"one-off" : false,
+				"serial" : null,
+		} ,
+		"effect" : {
+				"one": 0,
+				"two": 0,
+				"three": 0,
+				"four": 0,
+		}
+	},
+}
---
+++ scripts/card.gd
@@ -0,0 +1,20 @@
+extends PanelContainer
+
+var card_desc: Label
+var card_title: Label
+var card_image: TextureRect
+var cards = CardData.cards
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	card_desc = $MarginContainer/VBoxContainer/Label
+	card_title = $MarginContainer/VBoxContainer/Label2
+	card_image = $MarginContainer/VBoxContainer/TextureRect
+	setcard("002")
+	
+func setcard(card_key: String) -> void:
+	card_desc.text = cards[card_key]["description"]
+	card_title.text = cards[card_key]["name"]
+	card_image.texture = load(cards[card_key]["image"])
+	pass
+	
