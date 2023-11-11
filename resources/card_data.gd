b10200783242331c0b6ab9c0179b50b01f16eede
Przemyslaw Anton Arciszewski <przemyslawapl@gmail.com>
2023-11-11 15:35:41 +0100

    Gerði spila breytingu

--- resources/card_data.gd
+++ resources/card_data.gd
@@ -4,18 +4,9 @@ extends  Node
 #		"name" : "Text here",
 #		"description : "Text here",
 #		"image" : "path to image",
-#		"tags" : {
-#				"one-off" : true/false,
-#				"serial" : next card id or null,
-#		} ,
-#		"effect" : {
-#				"one": 0,
-#				"two": 0,
-#				"three": 0,
-#				"four": 0,
-#		}
+#		"tags" : { "one-off" : false, "serial" : null, },
+#		"effect" : { "one": 0, "two": 0, "three": 0, "four": 0, }
 #	},
-#}
 
 
 
@@ -27,30 +18,39 @@ var cards: Dictionary = {
 		"name" : "Joe",
 		"description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor viverra ex vel pretium. Donec nec porta leo. Phasellus pulvinar, sapien sed consectetur egestas, enim enim faucibus odio, eu semper.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor viverra ex vel pretium. Donec nec porta leo. Phasellus pulvinar, sapien sed consectetur egestas, enim enim faucibus odio, eu semper.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor viverra ex vel pretium. Donec nec porta leo. Phasellus pulvinar, sapien sed consectetur egestas, enim enim faucibus odio, eu semper.",
 		"image" : "res://assets/visual/Person_icon_BLACK-01.svg.png",
-		"tags" : {
-				"one-off" : false,
-				"serial" : null,
-		} ,
-		"effect" : {
-				"one": 0,
-				"two": 0,
-				"three": 0,
-				"four": 0,
-		}
+		"tags" : { "one-off" : false, "serial" : null, },
+		"effect" : { "one": 0, "two": 0, "three": 0, "four": 0, }
 	},
+	
 	"002" : {
 		"name" : "Mike",
 		"description" : "Ég fokking elska gæsir og geitur maður, þær eru það fallegasta í þessum heimi, sapien sed consectetur egestas, enim enim faucibus odio, eu semper.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor viverra ex vel pretium. Donec nec porta leo. Phasellus pulvinar, sapien sed consectetur egestas, enim enim faucibus odio, eu semper.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor viverra ex vel pretium. Donec nec porta leo. Phasellus pulvinar, sapien sed consectetur egestas, enim enim faucibus odio, eu semper.",
 		"image" : "res://assets/visual/output-onlinepngtools.png",
-		"tags" : {
-				"one-off" : false,
-				"serial" : null,
-		} ,
-		"effect" : {
-				"one": 0,
-				"two": 0,
-				"three": 0,
-				"four": 0,
-		}
+		"tags" : { "one-off" : false, "serial" : null, },
+		"effect" : { "one": 0,"two": 0, "three": 0, "four": 0, }
+	},
+	
+	"003" : {
+		"name" : "Text here",
+		"description" : "Text here",
+		"image" : "path to image",
+		"tags" : { "one-off" : false, "serial" : null, },
+		"effect" : { "one": 0, "two": 0, "three": 0, "four": 0, }
+	},
+	
+	"004" : {
+		"name" : "Text here",
+		"description" : "Text here",
+		"image" : "path to image",
+		"tags" : { "one-off" : false, "serial" : null, },
+		"effect" : { "one": 0, "two": 0, "three": 0, "four": 0, }
+	},
+	
+	"005" : {
+		"name" : "Text here",
+		"description" : "Text here",
+		"image" : "path to image",
+		"tags" : { "one-off" : false, "serial" : null, },
+		"effect" : { "one": 0, "two": 0, "three": 0, "four": 0, }
 	},
 }
